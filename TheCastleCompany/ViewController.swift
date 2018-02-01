32381bc234e7343fd68507cd9e32172e6b1b1d0b
Sanjay Shah <Sanjay Shah>
2018-01-31 19:53:43 -0800

    added basic UI

--- TheCastleCompany/ViewController.swift
+++ TheCastleCompany/ViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBOutlet weak var entryTextField: UITextField!
+    
+    @IBOutlet weak var addToArrayButton: UIButton!
+    
+    @IBOutlet weak var currentArrayLabel: UILabel!
+    
+    @IBOutlet weak var calculateButton: UIButton!
+    
+    @IBOutlet weak var castleCountLabel: UILabel!
+    
+    
+    var theEnteredArray: [Int]!
+    
     var castleCount: Int!
-    var lastHighPoint: Int!
-    var lastLowPoint: Int!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        theEnteredArray = []
+        currentArrayLabel.text = "Empty Array"
+        
         castleCount = 0
-        // Do any additional setup after loading the view, typically from a nib.
+        castleCountLabel.text = String(castleCount)
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,48 +46,99 @@ class ViewController: UIViewController {
     }
 
     
-    func myFunc(array: [Int]) -> Int {
+    func buildCastles(array: [Int]) -> Int {
         
-        var lastElement: Int = -10
+        var lastHighPoint: Int = -1
+        var lastLowPoint: Int = -1
+        
+        var lastElement: Int = 0
         var index: Int = 0
         
-        if (array.count>0) {
+        if (array.count == 1) {
             castleCount = 1
         }
         
-        for element in array{
-            if (element > lastElement){
-                print("we could be at a peak")
-                
-                if (array[lastLowPoint] == lastElement){
-                    castleCount = castleCount + 1
-                }
-                
-                lastHighPoint = index
-                
-            }
-            
-            else if (element < lastElement){
-                print("we could be at a valley")
-                
-                if (array[lastHighPoint] == lastElement){
-                    castleCount = castleCount + 1
-                }
-                
-                lastLowPoint = index
-                
-            }
-            
-            else {
-                print("we are at a flat")
-            }
         
+        for element in array{
+            if(lastElement == 0){
+                print("we are at the first element")
+                lastHighPoint = 0
+                lastLowPoint = 0
+            }
+            else {
+                if (element > lastElement){
+                    print("we could be at a peak")
+                    
+                    
+                    if (array[lastLowPoint] == lastElement){
+                        castleCount = castleCount + 1
+                        print("\(lastElement) is a valley")
+                    }
+                    
+                    lastHighPoint = index
+                    
+                }
+                    
+                else if (element < lastElement){
+                    print("we could be at a valley")
+                    
+                    if (array[lastHighPoint] == lastElement){
+                        castleCount = castleCount + 1
+                        print("\(lastElement) is a peak")
+                    }
+                    
+                    lastLowPoint = index
+                    
+                }
+                    
+                else {
+                    print("we are at a flat")
+                }
+            }
+            
             index += 1
             lastElement = element
         }
         
         return castleCount
     }
+    
+    
+    @IBAction func addToArray(_ sender: UIButton) {
+        
+        theEnteredArray.append(Int(entryTextField.text!)!)
+        
+        var currentArrayLabelString: String = ""
+        
+        for item in theEnteredArray{
+            currentArrayLabelString = currentArrayLabelString + ", \(String(item))"
+        }
+        
+        entryTextField.text = ""
+        currentArrayLabel.text = currentArrayLabelString
+        
+    }
+    
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        
+        castleCountLabel.text = String(buildCastles(array: theEnteredArray))
+        
+    }
+    
+    
+    @IBAction func reset(_ sender: UIButton) {
+        
+        theEnteredArray = []
+        currentArrayLabel.text = "Empty Array"
+        
+        castleCount = 0
+        castleCountLabel.text = String(castleCount)
+        
+    }
+    
+    
 
+    
 }
 
